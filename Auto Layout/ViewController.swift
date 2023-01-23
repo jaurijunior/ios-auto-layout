1303cd29fecced17ee084a274c2c5ebb8a945bcf
Jauri da Cruz Junior <jauri.dev@gmail.com>
2023-01-23 16:17:15 -0300

    create basic auto layout

---
+++ Auto Layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Auto Layout
+//
+//  Created by Jauri da Cruz Junior on 23/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
